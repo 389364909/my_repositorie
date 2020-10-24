53b9f546894d88950ec0e5adf661e6390f87fcd8
389364909 <389364909@qq.com>
2020-10-24 11:11:55 +0800

    抓取网页源代码

---
+++ 抓取网页源代码.py
@@ -0,0 +1,15 @@
+
+def catch(url):
+    import requests
+    code = (requests.get(url)).text
+    #print(code)
+    with open('code.txt','w') as code_:
+        code_.write(code)
+
+if __name__ == "__main__":
+    url_ = input("url:")
+    catch(url_)
+
+
+
+
